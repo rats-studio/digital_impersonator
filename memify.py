c963dfeb4182c2aa19cb7d7bc5ecb6f526cd1791
Kent Martin <kentaasvang@gmail.com>
2019-04-05 01:02:42 +0200

    fixed resizing of images, wrapping of text, os-dependent font(maybe) aaaaaaaand configured to not use images that are too small

--- memify.py
+++ memify.py
@@ -1,4 +1,5 @@
 import os
+import platform
 
 from random import randint
 import time
@@ -9,7 +10,7 @@ from PIL import ImageFont
 
 
 FONT_SIZE = 25
-LIMIT = 27
+LIMIT = 50
 BORDER_SIZE = 1
 MEMES_FOLDER = os.path.join("client", "static", "img", "memes")
 SAMPLE_PICS = os.path.join("client", "static", "img", "sample_img")
@@ -44,22 +45,53 @@ class Meme:
         self._draw_text()
         self.meme = Image.alpha_composite(self.img, self.canvas)
 
-    #TODO: need to break up long sentences into multiple lines
-    def _get_text_position(self, img, txt):
-        width, height = img.size
+    def _get_text_position(self):
+        """
+        should return list of tree-value tuple [(x, y, text)]
+        """
+        width, height = self.img.size
         width = width - width + 20
         height = height - self.font_size*2
+        ret_val = []
 
-        if len(txt) > LIMIT:
-            # break up sentence
-            pass
+        processing_text = self.text
 
-        return (width, height)
+        while True:
+            if len(processing_text) > self.limit:
+                s = self.find_split(processing_text)
+                ret_val.append((width, height, processing_text[:s]))
+                processing_text = processing_text[s:]
+                processing_text = processing_text.strip()
+            else:
+                ret_val.append((width, height, processing_text))
+                break
+        
+        sentences = len(ret_val)
+        real_ret_val = []  # omg this made me lol so hard. Super-hackerman
 
-    #TODO: different font for windows
+        for i in ret_val:
+            x, y = (i[0], i[1])
+            real_ret_val.append((x, y - (sentences*self.font_size), i[2]))
+            sentences -= 1
+
+        return real_ret_val
+
+    def find_split(self, text):
+        sub = 0
+        while text.find(" ", sub+1) < self.limit and text.find(" ", sub+1) != -1:
+            sub = text.find(" ", sub+1)
+
+        return sub 
+
     def get_font(self):
         try:
-            fnt = ImageFont.truetype(self.font_family, self.font_size)
+            # choice of font depends on which OS this is running on
+            o_sys = platform.system()
+            
+            if o_sys in ["Darwin", "Linux"]:
+                fnt = ImageFont.truetype(self.font_family, self.font_size)
+            if o_sys == "Windows":
+                fnt = ImageFont.truetupe("Pillow/Tests/fonts/Arial.ttf", self.font_size)
         except OSError as e:
             print("Couldn't find font, loading default. Meme might not display correctly")
             fnt = ImageFont.load_default() 
@@ -67,7 +99,7 @@ class Meme:
         return fnt
 
     def _resize_img(self, img):
-        if img.size[1] < 420:
+        if img.size[0] < 740:
             return img
         else:
             return self._resize_img(
@@ -76,37 +108,49 @@ class Meme:
             )
      
     def _get_random_image(self):
-        pics = len(os.listdir(self.sample_pics))  # how many pictures
-        random_pic = os.path.join(self.sample_pics, f"trump_{randint(0, pics-2)}.jpg")
-        im = Image.open(random_pic).convert("RGBA")
+
+        while True:
+            pics = len(os.listdir(self.sample_pics))  # how many pictures
+            random_pic = os.path.join(self.sample_pics, f"trump_{randint(0, pics-2)}.jpg")
+            im = Image.open(random_pic).convert("RGBA")
+            
+            # check that the image is not too small
+            if im.size[0] > 680:
+                break
+            
         return self._resize_img(im)
 
     def _draw_text(self):
-        self.draw.text(
-            self._get_text_position(self.img, self.text), 
-            self.text, 
-            font=self.get_font(), 
-            fill=(255,255,255,255)
-        )
+        text_pos = self._get_text_position()
+        
+        for i in range(len(text_pos)):
+            self.draw.text(
+                (text_pos[i][0], text_pos[i][1]), 
+                text_pos[i][2], 
+                font=self.get_font(), 
+                fill=(255, 255, 255, 255)
+            )
 
-    #TODO: this method is begging for a loop.
     def _draw_border(self):
-        x, y = self._get_text_position(self.img, self.text)
+        text_pos = self._get_text_position()
         font = self.get_font()
+    
+        for i in range(len(text_pos)):
+            x, y, text = text_pos[i]
 
-        self.draw.text((x-self.border_size, y), self.text, font=font, fill="black")
-        self.draw.text((x+self.border_size, y), self.text, font=font, fill="black")
-        self.draw.text((x, y-self.border_size), self.text, font=font, fill="black")
-        self.draw.text((x, y+self.border_size), self.text, font=font, fill="black")
+            self.draw.text((x-self.border_size, y), text, font=font, fill="black")
+            self.draw.text((x+self.border_size, y), text, font=font, fill="black")
+            self.draw.text((x, y-self.border_size), text, font=font, fill="black")
+            self.draw.text((x, y+self.border_size), text, font=font, fill="black")
 
-        self.draw.text(
-            (x-self.border_size, y+self.border_size), self.text, font=font, fill="black")
-        self.draw.text(
-            (x-self.border_size, y-self.border_size), self.text, font=font, fill="black")
-        self.draw.text(
-            (x+self.border_size, y+self.border_size), self.text, font=font, fill="black")
-        self.draw.text(
-            (x+self.border_size, y-self.border_size), self.text, font=font, fill="black")
+            self.draw.text(
+                (x-self.border_size, y+self.border_size), text, font=font, fill="black")
+            self.draw.text(
+                (x-self.border_size, y-self.border_size), text, font=font, fill="black")
+            self.draw.text(
+                (x+self.border_size, y+self.border_size), text, font=font, fill="black")
+            self.draw.text(
+                (x+self.border_size, y-self.border_size), text, font=font, fill="black")
 
     def display(self):
         self.meme.show()
@@ -115,11 +159,10 @@ class Meme:
         filename = f"{int(time.time())}.jpg"
         file_path = os.path.join(MEMES_FOLDER, filename)
         self.meme.convert("RGB").save(file_path)
-        print("LOOKIE HERE FROM memify", filename)
         return filename
 
 
 if __name__ == "__main__":
-    meme = Meme("this is just a test-string")
+    meme = Meme("this is just a test-string that is somewhat longer than the other one because yeah")
     meme.display()
 
